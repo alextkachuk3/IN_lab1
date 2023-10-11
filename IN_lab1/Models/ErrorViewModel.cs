1af09953820c52e38fd236b35aeb4fdc5f00a53a
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-10-11 19:17:42 +0300

    Add project files.

---
+++ IN_lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IN_lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
