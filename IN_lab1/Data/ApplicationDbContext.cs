047f9626e312b5abcf458e29b7111f730bc53065
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-10-12 18:12:51 +0300

    Setup MySQL database provider and user class

---
+++ IN_lab1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IN_lab1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Runtime;
+
+namespace IN_lab1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        { }
+
+        public DbSet<User>? Users { get; set; }
+
+    }
+}
---
+++ IN_lab1/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime;
+
+namespace IN_lab1.Models
+{
+    [Index(nameof(Username), IsUnique = true)]
+    public class User
+    {
+        public User() { }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            PasswordHash = password;
+        }
+
+        public bool CheckCredentials(string password)
+        {
+            return PasswordHash!.Equals(password);
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 4)]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? PasswordHash { get; set; }
+    }
+}
