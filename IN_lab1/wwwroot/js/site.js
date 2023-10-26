e8009c969c272f3957f1e7069a3de68e903c14b1
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-10-27 01:03:52 +0300

    Reload all opened pages after log in/sign up/log out

--- IN_lab1/wwwroot/js/site.js
+++ IN_lab1/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function IncreaseReloadCounter() {
+    localStorage.setItem('ReloadIteration', Number(localStorage.getItem('ReloadIteration')) + 1)
+}
 
-// Write your JavaScript code.
+let reload_iteration = localStorage.getItem('ReloadIteration')
+
+if (reload_iteration === null) {
+    localStorage.setItem('ReloadIteration', 1)
+}
+
+let sesion_reload_iteration = sessionStorage.getItem('ReloadIteration')
+
+if (sesion_reload_iteration === null) {
+    sessionStorage.setItem('ReloadIteration', localStorage.getItem('ReloadIteration'))
+}
+
+function checkReload() {
+    if (sessionStorage.getItem('ReloadIteration') < localStorage.getItem('ReloadIteration')) {
+        sessionStorage.setItem('ReloadIteration', localStorage.getItem('ReloadIteration'))
+        location.reload()
+    }
+}
+
+setInterval(checkReload, 1000);
