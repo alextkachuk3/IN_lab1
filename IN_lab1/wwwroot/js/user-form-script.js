e8abf4b12847dcf1aebc8507220134ad14cecd6c
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-10-26 22:54:22 +0300

    Shared login form inputs

---
+++ IN_lab1/wwwroot/js/user-form-script.js
@@ -0,0 +1,19 @@
+﻿$('#usernameinput').keypress(function (e) {
+    var regex = new RegExp("^[a-zA-Z0-9_]+$");
+    var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+    if (regex.test(str)) {
+        return true;
+    }
+    e.preventDefault();
+    return false;
+});
+
+$('#passwordinput').keypress(function (e) {
+    var regex = new RegExp("^[a-zA-Z0-9_]+$");
+    var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+    if (regex.test(str)) {
+        return true;
+    }
+    e.preventDefault();
+    return false;
+});
