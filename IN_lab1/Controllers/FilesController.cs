2436234baaabb31f9f99e87047d1159f09f10c0a
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-10-26 18:53:18 +0300

    Fix button width on low window resolution

--- IN_lab1/Controllers/FilesController.cs
+++ IN_lab1/Controllers/FilesController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
-using static System.Collections.Specialized.BitVector32;
 
 namespace IN_lab1.Controllers
 {
